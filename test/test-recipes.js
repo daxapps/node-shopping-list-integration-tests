aaac5bf338fa5dc4683e6c824d16c1f9f18e0810
Jason Crawford <daxapps777@gmail.com>
2017-06-05 13:11:27 -0500

    Create POST request test for Recipes

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -35,5 +35,22 @@ describe('Recipes', function() {
 			});
 	});
 
-	
+	it('should add an item on POST', function() {
+    const newItem = {name: 'PopCorn', ingredients: ['1 cup of popcorn kernals', 'pat of butter']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.id.should.not.be.null;
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+
+
 });
