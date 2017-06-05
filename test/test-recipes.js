d66c0c0a5fb4e7b5014d6013c1de1979fe8df152
Jason Crawford <daxapps777@gmail.com>
2017-06-05 13:01:13 -0500

    Create GET test for Recipes list

---
+++ test/test-recipes.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Recipes', function() {
+	before(function() {
+		return runServer();
+	});
+
+	after(function() {
+		return closeServer();
+	});
+
+	it ('should list items on GET', function() {
+		return chai.request(app)
+			.get('/recipes')
+			.then(function(res) {
+				res.should.have.status(200);
+				res.should.be.json;
+        res.body.should.be.a('array');
+
+        res.body.length.should.be.at.least(1);
+
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+        	item.should.be.a('object');
+        	item.should.include.keys(expectedKeys);
+        });
+
+			});
+	});
+
+	
+});
